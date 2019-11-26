8b730c2325d70e7a58385782463db6985e599ad0
takuya_okamoto <takuya_okamoto@kronos.jp>
2019-11-27 07:49:45 +0900

    add redux actioncreater

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+// アクションクリエイター
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+
+export const increment = () => ({
+  type: INCREMENT
+});
+
+export const decrement = () => ({
+  type: DECREMENT
+});
